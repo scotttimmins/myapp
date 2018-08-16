53547f602e79bf6c12578a413985085b255bb113
Bex <bexicles@gmail.com>
2018-08-16 16:38:47 +0100

    repointing app.js to html file

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ port = process.env.PORT;
 
 var express = require('express');
 var app = express();
-app.get('/hello', function (req, res) {
-    res.send('Hello JJ Frijoles Beans!');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.send('/dashboard.html');
 });
-app.get('/goodbye', function (req, res) {
-    res.send('Ciao banana!');
-});
+
 app.listen(port, function () {
     console.log('Example app listening on port' + port);
 });
