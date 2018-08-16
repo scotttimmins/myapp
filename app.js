cadf7dbae828ad7f0e3afbb18f2e5ef2b84b89a8
Bex <bexicles@gmail.com>
2018-08-16 14:19:30 +0100

    added port as environment variable

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+port = process.env.PORT;
+
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
     res.send('Hello JJ Frijoles Beans!');
 });
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+    console.log('Example app listening on port' + port);
 });
