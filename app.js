87a6c039d03506dbfc5b2c56af3daacbf342c30e
Bex <bexicles@gmail.com>
2018-08-16 16:43:31 +0100

    repointing app.js to html file

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function (req, res) {
+/*app.get('/hello', function (req, res) {
     res.send("hej!");
-});
+});*/
 
 app.listen(port, function () {
     console.log('Example app listening on port' + port);
