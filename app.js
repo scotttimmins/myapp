b2eb25529c4dd76e10c40b6e122d2ed957a7b143
Bex <bexicles@gmail.com>
2018-08-17 13:29:41 +0100

    refactored main page to be a handlebars page;
    added simple handlebars code to app.js

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ port = process.env.PORT;
 var express = require('express');
 var app = express();
 
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+
+
+// Register Handlebars view engine
+app.engine('handlebars', exphbs());
+// Use Handlebars view engine
+app.set('view engine', 'handlebars');
+
+
+
 app.use(express.static(__dirname + '/public'));
 
-/*app.get('/hello', function (req, res) {
-    res.send("hej!");
-});*/
+app.get('/', (req, res) => {
+    res.render('dashboard');
+});
 
 app.listen(port, function () {
     console.log('Example app listening on port' + port);
