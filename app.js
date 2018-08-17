3dcc16b30bbd05c6b3c19f6e0c8460543f696b0f
Bex <bexicles@gmail.com>
2018-08-17 14:38:38 +0100

    added subsequent placeholders for balances

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('view engine', 'handlebars');
 /**/app.use(express.static(__dirname + '/views'));
 
 app.get('/', (req, res) => {
-    res.render('dashboard', {name: 'Bex'});
+    res.render('dashboard', {name: 'Bex', bex_monzo: '52.06', peet_monzo: '66.43', bex_firstdirect: '150.23', peet_lloyds: '9,998.12', bex_barclaycard: '-500', peet_mbna1: '-9,786.99'});
 });
 
 app.listen(port, function () {
