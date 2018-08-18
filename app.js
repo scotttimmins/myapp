21a895f895a48e8e498f943a43c47ba66c568368
Bex <bexicles@gmail.com>
2018-08-18 11:13:04 +0100

    added redirect to dashboard once user signed in

--- app.js
+++ app.js
@@ -13,6 +13,31 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
 
+
+
+const GoogleStrategy = require('passport-google-    oauth').OAuth2Strategy;
+module.exports = (passport) => {
+    passport.serializeUser((user, done) => {
+        done(null, user);
+    });
+    passport.deserializeUser((user, done) => {
+        done(null, user);
+    });
+    passport.use(new GoogleStrategy({
+        clientID: 797266126678-p55b4276r50bgjv8cb3q3k3tv70f89s3.apps.googleusercontent.com,
+        clientSecret: HPcpFZSyBeS3I11B8vprwLxc,
+        callbackURL: https://my-money-dashboard.herokuapp.com/dashboard
+},
+    (token, refreshToken, profile, done) => {
+        return done(null, {
+            profile: profile,
+            token: token
+        });
+    }));
+};
+
+
+
 app.get('/', (req, res) => {
     res.render('index',);
 });
