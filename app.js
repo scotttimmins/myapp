6a9a8e096516366f22692be3e5d56462b3976d91
Bex <bexicles@gmail.com>
2018-08-18 12:39:22 +0100

    added redirect to dashboard once user signed in

--- app.js
+++ app.js
@@ -49,7 +49,8 @@ app.get('/auth/google', passport.authenticate('google', {
     scope: ['https://www.googleapis.com/auth/userinfo.profile']
 }));
 app.get('/auth/google/callback',
-    passport.authenticate('google', {failureRedirect:'/public'}),
+    passport.authenticate('google', {failureRedirect:'/public'
+    }),
     (req, res) => {
         req.session.token = req.user.token;
         res.redirect('dashboard', {name: 'Bex', bex_monzo: '52.06', peet_monzo: '66.43', bex_firstdirect: '150.23', peet_lloyds: '9,998.12', bex_barclaycard: '-500', peet_mbna1: '-9,786.99'});
@@ -69,11 +70,11 @@ app.get('/', (req, res) => {
 });
 */
 
-/*
+
 app.get('/dashboard', (req, res) => {
     res.render('dashboard', {name: 'Bex', bex_monzo: '52.06', peet_monzo: '66.43', bex_firstdirect: '150.23', peet_lloyds: '9,998.12', bex_barclaycard: '-500', peet_mbna1: '-9,786.99'});
 });
-*/
+
 
 app.listen(port, function () {
     console.log('Example server listening on port' + port);
