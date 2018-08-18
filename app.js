cdd543e32295306a38a035fcaabed3beff42cdcb
Bex <bexicles@gmail.com>
2018-08-18 14:12:25 +0100

    Redirect to login.html

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.get('/', (req, res) => {
 
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) { return next(); }
-    res.redirect('/login')
+    res.redirect('/login.html')
 }
 
 
